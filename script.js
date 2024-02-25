6012d56b20d5ed434bc98920b1d49f8491a0be14
priyarai05 <pri2605@gmail.com>
2024-02-25 19:32:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const expression = document.querySelector('#exp')
+
+const numbers = document.querySelectorAll('.number')
+
+numbers.forEach(text => {
+    text.addEventListener('click', function () {
+        var expressionVal = expression.value;
+        console.log(text.innerHTML);
+        if (text.innerHTML === 'RESET') {
+            expression.value = "";
+        }
+        else if (text.innerHTML === 'DEL') {
+            expression.value = expressionVal.substring(0, expressionVal.length - 1)
+        }
+        else if (text.innerHTML === '=') {
+            let myPromise = new Promise((resolve, reject) => {
+                var expressionVal2 = expression.value
+                if (expressionVal2.includes('x')) {
+                    expressionVal2 = expressionVal2.replaceAll("x", '*')
+                }
+                console.log(expressionVal2)
+                expression.value = eval(expressionVal2)
+            })
+            myPromise.then().catch((e) => {
+                console.log("Invalid Input");
+            })
+        }
+        else {
+            expression.value += text.innerHTML;
+        }
+
+    })
+})
